3a4de82e7a82c4690ab26c47735c9dd5f5cc36dc
Ezzat Eissa <eeissa@nama.net.sa>
2020-09-08 16:40:50 +0300

    change env url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:9000/admin'
+  apiUrl: 'http://jwt-auth-server-git-consent-auth-server.riyadbank-us-east-3-bx2-296b1aa3b5f384c7c2983c589dc3a273-0000.us-east.containers.appdomain.cloud/admin'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:9000/admin'
+  apiUrl: 'http://jwt-auth-server-git-consent-auth-server.riyadbank-us-east-3-bx2-296b1aa3b5f384c7c2983c589dc3a273-0000.us-east.containers.appdomain.cloud/admin'
 };
